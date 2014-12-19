7ca5933c7bd48f1650eca57dc6d999ff30543845
Manuel Schmalstieg <git.code@ms-studio.net>
2014-12-19 22:21:29 +0100

    mini calendar and archive post type.

--- inc/mini-calendar.php
+++ inc/mini-calendar.php
@@ -1,32 +1,117 @@
-<B_retest_age>
-<BOUCLE_retest_age(ARTICLES){id_article}{age<1}> 
-</BOUCLE_retest_age>
-	<?php 
+<?php 
+
+	// 1) We need to test for the current date.
+		
+	if (function_exists('mem_date_of_today')) {
+
+		$c12_date_of_today = mem_date_of_today();
+		
+		// echo $c12_date_of_today["today-now"];
+
+	} else {
+	// MEM plugin not active
+		$c12_date_of_today = '';
+	}
 	
-	// test if current an article has a current event date
+	// 2) Query for posts that have start_date in allowed range
+	
+	if (!empty($c12_date_of_today)) {
 			
-	// if yes: output CSS and mini calendar
-	
-	 ?>
-
-		<BOUCLE_article_courant(ARTICLES){id_article}>
-				<style>.concert-no-#ID_ARTICLE a {
-					background: #ce0000;
-					color:#fff;
-					box-shadow: inset 0 0 8px rgb(122, 11, 11);
-				}
-				.concert-no-#ID_ARTICLE a:hover {
-					color: #fff;
-				}
+			$nfo_unix_year = (  1 * 1 * 24 * 60 * 60 );
+			
+			// check ID of current article:
+			$c12_item_id = get_the_ID();
+			
+			// 3) Check for transient
+			
+			if ( false === ( $c12_minical_events = get_transient( 'c12_minical_events' ) ) ) {
+			    
+			    // It wasn't there, so we generate the data and save the transient
+			    
+			    $c12_unix_now = $c12_date_of_today["unix"];
+			    $c12_unix_1day = ( 1 * 24 * 60 * 60 ); // 3 jours
+			    
+			    $c12_unix_yesterday = ( $c12_unix_now - $c12_unix_1day );
+			    $c12_date_yesterday = date_i18n( "Y-m-d", $c12_unix_yesterday);
+			
+			     $c12_minical_events = new WP_Query( array(
+			     	'posts_per_page' => 25,
+			     	'meta_key' => '_mem_start_date',
+			     	'meta_value'	=> $c12_date_yesterday,
+			     	'meta_compare'	=> '>=',
+			     	'orderby'  => 'meta_value',
+			     	'order'  => 'ASC',
+			     //'cat' => '10,12,13,14,18', 
+			     	 	) ); 
+			     	 	
+			     	 	set_transient(
+			     	 		'c12_minical_events', 
+			     	 		$c12_minical_events, 
+			     	 		10 
+			     	 	); // 3 heures = 60*60*3
+			
+			} // end of get_transient test
+			
+			// 4) We have defined $c12_minical_events
+			// - now, generate output
+			
+			
+			
+			if ( $c12_minical_events->have_posts() ) : ?>
+			  
+			  <style>.concert-no-<?php echo $c12_item_id; ?> a {
+			  	background: #ce0000;
+			  	color:#fff;
+			  	box-shadow: inset 0 0 8px rgb(122, 11, 11);
+			  }
+			  .concert-no-<?php echo $c12_item_id; ?> a:hover {
+			  	color: #fff;
+			  }
+			  
+			  </style>
+			  <ul class="mini-cal">
+			  
+			  <?php
+			  while( $c12_minical_events->have_posts() ) : $c12_minical_events->the_post(); 
+			  
+						$current_post_id = get_the_ID();
+						
+				  	$c12_item_date = mem_date_processing( 
+				  		get_post_meta($current_post_id, '_mem_start_date', true) , 
+				  		get_post_meta($current_post_id, '_mem_end_date', true)
+				  	);
+			  
+			  ?>
+					
+					<li class="mini-cal-box concert-no-<?php echo $current_post_id; ?>">
+						<a href="<?php the_permalink(); ?>" title="<?php the_title(); ?>">
+						<?php 
+						
+						$c12_item_day_nr = date( "d", $c12_item_date["start-unix"] );
+						
+						if (1 == $c12_item_day_nr) {
+						
+							echo '1<sup class="sup">er</sup>';
+							
+						} else {
+						
+							echo $c12_item_day_nr;
+						}
+						
+						 ?>
+						</a></li>
+					
+			    <?php
+			  endwhile; 
 				
-				</style>
-		</BOUCLE_article_courant>
+				?></ul><?php
+				
+			 wp_reset_postdata();
+			endif; 
+			
+			
 		
-		<INCLURE{fond=inc-minical}>
-		
-</B_retest_age>
+	} // end testing if $c12_date_of_today is empty.
+	
 
-[(#ENV{contexte}|=={sommaire}|?{
-	[(#INCLURE{fond=inc-minical})],
-	''
-})]
+ ?>
--- single-archive.php
+++ single-archive.php
@@ -15,10 +15,135 @@ get_header(); ?>
    	<div class="surlignable">
    	
     <header>
+    
+    	<?php 
+    	
+    	// on crée une liste des archives
+    	
+    	if ( false === ( $c12_archive_titles = get_transient( 'c12_archive_titles' ) ) ) {
+    	    
+    	    // It wasn't there, so we generate the data and save the transient
+    	
+    	     $c12_archive_titles = new WP_Query( array(
+    	     	'posts_per_page' => 55,
+    	     	'post_type' => 'archive',
+    	     	'orderby'  => 'name',
+    	     	'order'  => 'ASC',
+    	     	 	) ); 
+    	     	 	
+    	     	 	set_transient(
+    	     	 		'c12_archive_titles', 
+    	     	 		$c12_archive_titles, 
+    	     	 		10 
+    	     	 	); // 3 heures = 60*60*3
+    	
+    	} // end of get_transient test
+    	
+    	
+    	if ( $c12_archive_titles->have_posts() ) : ?>
+    	  <ul class="top-archive separateurs notc">
+    	  <?php
+    	  while( $c12_archive_titles->have_posts() ) : $c12_archive_titles->the_post(); 
+    			
+    			echo '<li><a href="'.get_the_permalink().'">';
+    			echo get_the_title().'</a></li>';
+    	
+    		 endwhile; 
+    	 	?></ul><?php
+    	  wp_reset_postdata();
+    	 endif; 
+    	 
+    	?>
+    	
       <h1 class="h1"><?php the_title(); ?></h1>
+      
     </header>
   	
     <?php the_content('<p class="serif">Read the rest of this page &raquo;</p>'); ?>
+    
+    <?php 
+    
+    // auto-display posts of current year.
+    
+    // get slug of current page: 
+    $c12_archive_slug = $post->post_name;
+    
+    if ( false === ( $c12_archive_year = get_transient( 'c12_archive_year_'.$c12_archive_slug ) ) ) {
+        
+        // It wasn't there, so we generate the data and save the transient
+        $c12_archive_year_start = $c12_archive_slug;
+        $c12_archive_year_end = $c12_archive_slug.'-12-32';
+    
+         $c12_archive_year = new WP_Query( array(
+         	'posts_per_page' => 999,
+         	'meta_key' => '_mem_start_date',
+         	'meta_value'	=> array( 
+         			$c12_archive_year_start, 
+         			$c12_archive_year_end ),
+         	'meta_compare'	=> 'BETWEEN',
+         	'orderby'  => 'meta_value',
+         	'order'  => 'ASC',
+         //'cat' => '10,12,13,14,18', 
+         	 	) ); 
+         	 	
+         	 	set_transient(
+         	 		'c12_archive_year_'.$c12_archive_slug, 
+         	 		$c12_archive_year, 
+         	 		10 
+         	 	); // 3 heures = 60*60*3
+    
+    } // end of get_transient test
+    
+    
+    if ( $c12_archive_year->have_posts() ) : ?>
+      
+      <ul class="articles">
+      
+      <?php
+      while( $c12_archive_year->have_posts() ) : $c12_archive_year->the_post(); 
+      
+    			$current_post_id = get_the_ID();
+    			
+    	  	$mem_date = mem_date_processing( 
+    	  		get_post_meta($current_post_id, '_mem_start_date', true) , 
+    	  		get_post_meta($current_post_id, '_mem_end_date', true)
+    	  	);
+      
+      ?>
+    		
+    			<li>
+    				<?php 
+    				
+    				if ( ! has_excerpt() ) {
+    				      echo '';
+    				} else { 
+    				
+//    							echo '<strong>';
+//    				      the_excerpt();
+//    				     	echo '</strong><br/>';
+    				}
+    				
+    				 ?>
+    			  <span class="date"><?php 
+    			  
+    			  echo date_i18n( "j", $mem_date["start-unix"]);
+    			  echo ' ';
+    			  echo date_i18n( "F", $mem_date["start-unix"]); ?></span> – 
+    			   <a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
+    			</li>
+    		
+        <?php
+      endwhile; 
+    	
+    	?></ul><?php
+    	
+     wp_reset_postdata();
+    endif; 
+    
+    
+     ?>	
+    
+    
     </div>
 
   </article>
