cffafe5a4614de469e64581d6967738e8607953d
Manuel Schmalstieg <git.code@ms-studio.net>
2018-04-05 20:46:02 +0200

    fonctionnement FrontPage
    fixes issue #3

---
+++ functions/pre-get-posts.php
@@ -0,0 +1,75 @@
+<?php 
+
+/*
+* Pre-Get Posts
+* Change display of posts loop
+ */
+ 
+ 
+
+/*
+ * Show Scheduled articles:
+ * http://wordpress.stackexchange.com/questions/16794/show-scheduled-posts-in-archive-page
+ *
+******************************/
+
+function cave12_include_future( $query ) {
+    if ( $query->is_date() || $query->is_single() )
+        $GLOBALS[ 'wp_post_statuses' ][ 'future' ]->public = true;
+}
+
+if ( !is_admin() ) {
+	add_filter( 'pre_get_posts', 'cave12_include_future' );
+}
+
+
+function c12_date_yesterday() {
+	
+	$c12_unix_now = strtotime( date("Y-m-d") );
+	$c12_unix_1day = ( 1 * 24 * 60 * 60 ); // 3 jours
+	
+	$c12_unix_yesterday = ( $c12_unix_now - $c12_unix_1day );
+	$c12_date_yesterday = date_i18n( "Y-m-d", $c12_unix_yesterday);
+	
+	return $c12_date_yesterday;
+	
+}
+
+/*
+ * Change query for front page
+ *
+******************************/
+
+function cave12_frontpage_concerts( $query ) {
+            
+        if ( $query->is_home() && $query->is_main_query() ) {
+        	
+        	$query->set('posts_per_page', -1);
+        	
+        	$query->set('post_status', array( 'publish', 'future' ));
+        	
+        	$query->set('date_query', array(
+      			array(
+      				'after'     => date('Y-m-d', strtotime('-60 days')),
+      				'inclusive' => true,
+      			),
+        	));
+        	
+        	$c12_date_yesterday = c12_date_yesterday();
+        	
+        	$query->set('meta_query', array(
+        			array(
+        				'key'     => '_mem_start_date',
+        				'value'   => $c12_date_yesterday,
+        				'compare' => '>=',
+        			),
+        	));
+        	
+        	$query->set('order', 'ASC');
+        	$query->set('orderby', 'meta_value');
+        	
+        	return $query;
+					
+        }
+}
+add_filter( 'pre_get_posts', 'cave12_frontpage_concerts' );
