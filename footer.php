c41b21c6a0f8e0d2cd040fefbb89aa21a6841710
Manuel Schmalstieg <git.code@ms-studio.net>
2018-06-23 18:43:05 +0200

    retrait des statistiques Piwik
    on va se contenter des statistiques Infomaniak

--- footer.php
+++ footer.php
@@ -58,19 +58,6 @@
   
 </div><!--#page-->
 
-	<!-- Piwik -->
-	<script type="text/javascript">
-	var pkBaseURL = (("https:" == document.location.protocol) ? "https://www.cave12.org/piwik/" : "http://www.cave12.org/piwik/");
-	document.write(unescape("%3Cscript src='" + pkBaseURL + "piwik.js' type='text/javascript'%3E%3C/script%3E"));
-	</script><script type="text/javascript">
-	try {
-	var piwikTracker = Piwik.getTracker(pkBaseURL + "piwik.php", 1);
-	piwikTracker.trackPageView();
-	piwikTracker.enableLinkTracking();
-	} catch( err ) {}
-	</script><noscript><p><img src="https://www.cave12.org/piwik/piwik.php?idsite=1" style="border:0" alt="" /></p></noscript>
-	<!-- End Piwik Tracking Code -->
-
   <?php wp_footer(); ?>
 
 </body>
