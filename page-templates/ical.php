e0968a7d63714bdfb253a453d62f3f6716f2f335
Manuel Schmalstieg <git.code@ms-studio.net>
2018-12-06 21:29:03 +0100

    better comment syntax

--- page-templates/ical.php
+++ page-templates/ical.php
@@ -8,16 +8,16 @@ header('Content-Disposition: attachment; filename="ical.ics"');
  
 /*
 
-infos about iCalendar file format:
-http://en.wikipedia.org/wiki/ICalendar
+* infos about iCalendar file format:
+* http://en.wikipedia.org/wiki/ICalendar
 
-RFC 5545 replaced RFC 2445 in September 2009 and now defines the standard.
-http://tools.ietf.org/html/rfc5545
+* RFC 5545 replaced RFC 2445 in September 2009 and now defines the standard.
+* http://tools.ietf.org/html/rfc5545
 
 */
 
 echo cave12_ical_output();
 
-/**
+/*
  * end of file
- */
+*/
