147668fa936ffb2ef2f42785e83bb895938c55cc
Manuel Schmalstieg <git.code@ms-studio.net>
2022-12-04 19:37:07 +0100

    petit correctif style des menus

--- functions/init.php
+++ functions/init.php
@@ -33,7 +33,7 @@ function custom_register_styles() {
 			'c12',
 			get_stylesheet_directory_uri() . '/css/dev/00-main.css', // main.css
 			false, // dependencies
-			'20221129a' // version
+			'20221204a' // version
 	);
 
 	// remove some plugin CSS:
