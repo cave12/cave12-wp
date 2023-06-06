c60de735e53195cd73a45c85a83c0e4e1ae7ae71
Manuel Schmalstieg <git.code@ms-studio.net>
2023-06-06 09:52:37 +0200

    gestion des affiches sans miniature
    Certaines affiches (10 affiches PDF de 2012) n'ont pas de miniature pour une raison inconnue.
    
    Ce code vérifie ce cas de figure. On n'affiche que les images qui ont une miniature.

--- functions/get-content/affiches.php
+++ functions/get-content/affiches.php
@@ -69,86 +69,102 @@ function c12_affiches( $auteur ) {
   			$id = get_the_ID();
   			
   			$pdf = wp_get_attachment_url( $id );
-  			
-  			$html .= '<figure class="affiche">';
-  			$html .= '<a href="'; 
-  			// $html .= wp_get_attachment_url( $id ) .'"'; 
-  			// get width and height data attributes!
-  			$c12_affiche_src = wp_get_attachment_image_src(
+
+			$c12_affiche_src = wp_get_attachment_image_src(
   				$id, 
   				'large'
   			);
-  			$html .= $c12_affiche_src[0] .'"'; // URL
-  			$html .= ' data-lbwps-width="'.$c12_affiche_src[1] .'"'; 
-  			$html .= ' data-lbwps-height="'.$c12_affiche_src[2] .'"'; 
-  			
-  			// Add date, in order to debug things.
-  			$html .= ' data-date="'.$post->post_date.'"';
-  			
-  			$html .= '>';
 
-				$html .= wp_get_attachment_image(
-					$id, // Image attachment ID
-					'medium', // valid image size, or an array of width and height values in pixels
-					false, // Whether the image should be treated as an icon
-					'' // Attributes for the image markup
-				);
-					
-				$html .= '</a>';
-					
-				$html .= '<figcaption class="affiche-meta">'; 
-					
-					// Concert lié?
-					
-					// 1) Tester le custom field:
-					// c12_spip_linked_article 
-					// (pour concerts importés depuis SPIP)
-					
-					if ( get_post_meta( $id, 'c12_spip_linked_article', true ) ) {
-										
-						$spip_id = get_post_meta( 
-							$id, 'c12_spip_linked_article', true ); 
-						
-						$html .= c12_linked_spip_article( $spip_id );
+			// echo '<pre>';
+			// 	var_dump($c12_affiche_src);
+			// echo '</pre>';
 
-					} else {
-					
-						// 2) Tester la relation enfant - parent:
+			if(is_array($c12_affiche_src)){
+  			
+				$html .= '<figure class="affiche">';
+				$html .= '<a href="'; 
+				// $html .= wp_get_attachment_url( $id ) .'"'; 
+				// get width and height data attributes!
+	
+				$html .= $c12_affiche_src[0] .'"'; // URL
+				$html .= ' data-lbwps-width="'.$c12_affiche_src[1] .'"'; 
+				$html .= ' data-lbwps-height="'.$c12_affiche_src[2] .'"'; 
+				
+				// Add date, in order to debug things.
+				$html .= ' data-date="'.$post->post_date.'"';
+				
+				$html .= '>'; // close the <a href opening tag
+
+					$html .= wp_get_attachment_image(
+						$id, // Image attachment ID
+						'medium', // valid image size, or an array of width and height values in pixels
+						false, // Whether the image should be treated as an icon
+						'' // Attributes for the image markup
+					);
 						
-						$parent_id = $post->post_parent;
+					$html .= '</a>';
 						
-						if ( $parent_id ) {
+					$html .= '<figcaption class="affiche-meta">'; 
+						
+						// Concert lié?
+						
+						// 1) Tester le custom field:
+						// c12_spip_linked_article 
+						// (pour concerts importés depuis SPIP)
+						
+						if ( get_post_meta( $id, 'c12_spip_linked_article', true ) ) {
+											
+							$spip_id = get_post_meta( 
+								$id, 'c12_spip_linked_article', true ); 
 							
-							$html .= c12_linked_article( $parent_id );
+							$html .= c12_linked_spip_article( $spip_id );
+
+						} else {
 						
+							// 2) Tester la relation enfant - parent:
+							
+							$parent_id = $post->post_parent;
+							
+							if ( $parent_id ) {
+								
+								$html .= c12_linked_article( $parent_id );
+							
+							}
+							
 						}
 						
-					}
-					
-					$html .= '<div class="extra">';
-					
-					// Vérifier format (PDF ou JPG?)
-					$mime_type = get_post_mime_type( $id );
-										
-					if ( $mime_type == "application/pdf" ) {
-					
-						$html .= '<a href="'; 
-						$html .= wp_get_attachment_url( $id ); 
-						$html .= '">PDF</a>';
+						$html .= '<div class="extra">';
 						
-					}
+						// Vérifier format (PDF ou JPG?)
+						$mime_type = get_post_mime_type( $id );
+											
+						if ( $mime_type == "application/pdf" ) {
+						
+							$html .= '<a href="'; 
+							$html .= wp_get_attachment_url( $id ); 
+							$html .= '">PDF</a>';
 							
-					if ( current_user_can( 'edit_others_pages' ) ) {
-//						$html .= ' ( <a href="'; 
-//						$html .= admin_url('upload.php?item='.$id);
-//						$html .= '">edit</a> )';
-					}
+						}
+								
+						if ( current_user_can( 'edit_others_pages' ) ) {
+	//						$html .= ' ( <a href="'; 
+	//						$html .= admin_url('upload.php?item='.$id);
+	//						$html .= '">edit</a> )';
+						}
+						
+						$html .= '</div>';
 					
-					$html .= '</div>';
-				
-				$html .= '</figcaption>';
-		
-			$html .= '</figure>';
+					$html .= '</figcaption>';
+			
+				$html .= '</figure>';
+			}  else { // verification "is_array"
+
+				$html .= "<!-- error for ";
+				$html .= $pdf;
+
+				$html .= " -->";
+
+			}   // end verification "is_array"
 
     endwhile; 
   	
